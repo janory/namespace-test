2e36a1ed8d67c735d8c991d0d756d5511bf9fc18
János Szathmáry <29057504+janory@users.noreply.github.com>
2024-06-24 16:24:44 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,14 +16,6 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
           </a>
         </div>
       </div>
@@ -53,7 +45,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            features and API.
           </p>
         </a>
 
